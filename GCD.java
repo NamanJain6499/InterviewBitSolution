6b1a7e0ac91178073077af9d0626ee7750352c35
NamanJain6499 <43249270+NamanJain6499@users.noreply.github.com>
2021-01-25 23:20:37 +0530

    Add files via upload

---
+++ GCD.java
@@ -0,0 +1,18 @@
+package com.java.practice;
+
+public class GCD {
+    public static void main(String args[]){
+        int a=10,b=15;
+        GCD parameter=new GCD();
+        int c=parameter.gcd(a,b);
+        System.out.println(c);
+    }
+
+    private int gcd(int A, int B) {
+        if(B==0){
+            return A;
+
+        }
+        return gcd(B,A%B);
+    }
+}
